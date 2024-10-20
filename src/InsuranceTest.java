caeb1fe73977a58371c43edd00935a54a2f47900
Jess-Do <jdo5@students.kennesaw.edu>
2024-10-20 16:16:24 -0400

    finished the structure of the driver class (still need to add contents from the sub-classes)

--- src/InsuranceTest.java
+++ src/InsuranceTest.java
@@ -1,31 +1,63 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InsuranceTest {
     public static void main(String[] args) {
+        ArrayList<InsurancePolicy> policies = new ArrayList<>();
         Scanner userinput = new Scanner(System.in);
-        while (true) {
+
+        int option = 0;
+
+        while (option != 3) {
             System.out.println("1 - Create Health Insurance Policy");
             System.out.println("2 - Create Term-Life Insurance Policy");
             System.out.println("3 - Exit \n");
             System.out.println("Enter Choice: ");
 
-            int option = userinput.nextInt();
+            if (userinput.hasNextInt()) {
+                option = userinput.nextInt();
 
-            if (option == 1) {
-                //Input funcution call for option 1
+                if (option == 1) {
+                    System.out.println("Enter name of policy holder: ");
+                    String name = userinput.next();
+                    System.out.println("Enter deductible Amount: ");
+                    double deductibleAmount = userinput.nextDouble();
+                    System.out.println("Enter co-Payment: ");
+                    double coPayment = userinput.nextDouble();
+                    System.out.println("Enter total out-of-pocket amount: ");
+                    double totalOutOfPocket = userinput.nextDouble();
 
+                    //health insurance object to policies list
 
-            } else if (option == 2) {
-                //Input function call for option 2
-            } else if (option == 3) {
-                //Input function call for option 3
+                } else if (option == 2) {
+                    System.out.println("Enter name of policy holder: ");
+                    String name = userinput.next();
+                    System.out.println("Enter name of beneficiary: ");
+                    String beneficiary = userinput.next();
+                    System.out.println("Enter number of years in term: ");
+                    int term = userinput.nextInt();
+                    System.out.println("Enter amount of payout: ");
+                    double amount = userinput.nextDouble();
+
+                    //term-Life insurance object to policies list
+
+                } else if (option == 3) {
+                    break;
+
+                } else {
+                    System.out.println("Error: Please enter valid input");
+                    System.out.println(" ");
+                }
+
             } else {
                 System.out.println("Error: Please enter valid input");
-
+                System.out.println(" ");
+                userinput.next();
             }
         }
 
+        // print the contents of the insurance list
+
+        userinput.close();
     }
-
-
 }
